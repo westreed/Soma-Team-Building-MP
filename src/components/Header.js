181a19debd44d85bc65153640199d03f7f330d83
JangSeHun <westreed@naver.com>
2023-04-23 22:22:50 +0900

    full change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Logo from '../images/logo.png';
+import Title from '../images/title.png';
 import "../css/header.css";
 
 function Header(){
     return (
-        <div className="soma-header">
-            <h1 className='soma-title'>SomaHub</h1>
+        <div className="soma-header" style={{display:"flex", flexDirection:"row", justifyContent:"space-between", flexWrap: "wrap"}}>
+            <div>
+                <nav style={{display:"flex", flexDirection:"row"}}>
+                    <img src={Logo} alt="Logo" style={{ objectFit:"contain"}} width="100px" />
+                    <p className='soma-title'>SomaHub</p>
+                </nav>
+                <div className='soma-header-title'>
+                    <p>SW 마에스트로 연수생들끼리</p>
+                    <p>팀빌딩을 편하게!</p>
+                </div>
+                <div className='soma-header-content' style={{marginBottom:"2rem"}}>
+                    <p style={{marginBottom:"0.7rem"}}>지금 바로 글을 작성해보세요.</p>
+                    <button className='button-style'>글쓰러 가기 →</button>
+                </div>
+            </div>
+            <img src={Title} alt="Title" style={{ objectFit:"contain", maxWidth: '500px', width: '100%'}} />
         </div>
     );
 }
